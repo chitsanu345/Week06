d3ecada7a7d896b3a1e8c31ff1b64f7a6fc301e9
chitsanu345 <chitsanu345@gmail.com>
2017-09-15 09:46:32 +0700

    P02

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -1,15 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Lab01
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        sayHello();
+        Console.ReadLine();
+    }
+
+    static void sayHello()
+    {
+        Console.WriteLine("Hello World.");
     }
 }
+
