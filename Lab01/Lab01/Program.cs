f44cfa14c035ee6934fe990b414e1c6238e69fa0
chitsanu345 <chitsanu345@gmail.com>
2017-09-15 10:12:32 +0700

    P09

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -1,15 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Lab01
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        int val = 999;
+        refMethod(out val);
+        Console.WriteLine(val);
+        Console.ReadLine();
+    }
+
+    static void refMethod(out int i)
+    {
+        i = i + 1;
     }
 }
+
