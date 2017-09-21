fd38b5117e4df69777b3f54cbd42f2783f6e480f
chitsanu345 <chitsanu345@gmail.com>
2017-09-21 23:28:19 +0700

    P17

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -10,6 +10,12 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+            string hello = "Hello";
+            string world = "World";
+            string helloWorld = hello + world;
+            Console.WriteLine(helloWorld);
+            string helloWorld555 = helloWorld + " " + 555;
+            Console.WriteLine(helloWorld555);
         }
     }
 }
