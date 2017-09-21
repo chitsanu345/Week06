c009e95b63b31d9fb22fa402672dc46a81662637
chitsanu345 <chitsanu345@gmail.com>
2017-09-21 23:26:04 +0700

    P16

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -10,6 +10,16 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+            byte a = 3;
+            byte b = 5;
+
+            Console.WriteLine(a | b);
+            Console.WriteLine(a & b);
+            Console.WriteLine(a ^ b);
+            Console.WriteLine(~a & b);
+            Console.WriteLine(a << 1);
+            Console.WriteLine(a << 2);
+            Console.WriteLine(a >> 1);
         }
     }
 }
