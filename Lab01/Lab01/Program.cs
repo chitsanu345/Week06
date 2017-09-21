5880f57bb36c0f7ae0b93e3fa370eafb51b3575b
chitsanu345 <chitsanu345@gmail.com>
2017-09-21 23:48:19 +0700

    P24

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -10,6 +10,11 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+            int a = 2;
+            float f = 3.5f;
+            double d = 4.99d;
+            decimal money = 1254841354;
+            Console.WriteLine(a + " " + f + " " + d + " " + money);
         }
     }
 }
