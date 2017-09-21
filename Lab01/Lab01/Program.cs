fa0b1f381afbc7579b1fd956095d69a70fdd3544
chitsanu345 <chitsanu345@gmail.com>
2017-09-21 23:22:25 +0700

    P15

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -10,6 +10,17 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+            bool a = true;
+            bool b = false;
+            Console.WriteLine(a && b);
+            Console.WriteLine(a || b);
+            Console.WriteLine(!b);
+            Console.WriteLine(b || true);
+            Console.WriteLine((9 > 7) ^ (a == b));
+            Console.WriteLine((b != a) || (a == b));
+            Console.WriteLine((b != a) && (a == b));
+            Console.WriteLine((!a) && (!b));
+            Console.WriteLine((!a) || (!b));
         }
     }
 }
