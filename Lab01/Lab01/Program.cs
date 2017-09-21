04bbc2b1561f3488fe52735d9cc67ef06c047d8b
chitsanu345 <chitsanu345@gmail.com>
2017-09-21 23:14:20 +0700

    P14

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -10,6 +10,13 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+            int x = 10, y = 5;
+            Console.WriteLine("x > y : " + (x > y));
+            Console.WriteLine("x < y : " + (x < y));
+            Console.WriteLine("x >= y : " + (x >= y));
+            Console.WriteLine("x <= y : " + (x <= y));
+            Console.WriteLine("x == y : " + (x == y));
+            Console.WriteLine("x != y : " + (x != y));
         }
     }
 }
