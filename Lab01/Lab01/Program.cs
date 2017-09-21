7441b1c1e630793092089477d1c03aefc76d6716
chitsanu345 <chitsanu345@gmail.com>
2017-09-21 23:43:00 +0700

    P22

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -10,6 +10,11 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+            double d = 5e9d;
+            Console.WriteLine(d);
+            int i = checked((int)d);
+            Console.WriteLine(i);
+
         }
     }
 }
