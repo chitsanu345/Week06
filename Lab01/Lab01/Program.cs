eb51fc78f324c8b5f27a86c28b1e5b302518e91c
chitsanu345 <chitsanu345@gmail.com>
2017-09-21 23:37:42 +0700

    P21

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -10,6 +10,10 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+            double d = 5e9d;
+            Console.WriteLine(d);
+            int i = (int)d;
+            Console.WriteLine(i);
         }
     }
 }
